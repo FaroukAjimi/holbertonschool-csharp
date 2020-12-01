50f5a04c45fdf44d4a5e0b75cf036b08e6cd4cc1
FaroukAjimi <benajimi@gmail.com>
2020-12-01 15:36:38 +0100

    Soo

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            for (int i = 0; i <= 79; i++)
+            {
+            if (i % 10 == 0)
+               {
+                    y = y + 1;
+                    i = i + y;
+                }
+            Console.Write("{0:00}, ", i);
+            }
+            Console.WriteLine("89");
+        }
+    }
+}
