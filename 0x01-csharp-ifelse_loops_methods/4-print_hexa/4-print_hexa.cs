5fb18e87e4e4ca95c484d0a90a9987ecd8e0a1e6
FaroukAjimi <benajimi@gmail.com>
2020-12-01 15:03:15 +0100

    So

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++)
+                Console.WriteLine("0x{0}", Convert.ToString(i, 16));
+        }
+    }
+}
