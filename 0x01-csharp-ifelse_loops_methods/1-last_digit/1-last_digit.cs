9d76dcd870360d3a9709a46fc97febae527bdc37
FaroukAjimi <benajimi@gmail.com>
2020-12-01 12:32:12 +0100

    Second task

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        Console.Write("The las digit of {0} is ", number);
+        number = number % 10;
+        Console.Write(number);  
+        if (number > 5)
+            {
+                Console.WriteLine(" and is greater than 5");
+            }
+        else if (number == 0)
+            {
+                Console.WriteLine(" and is 0");
+            }
+        else
+            {
+                Console.WriteLine(" and is less than 6 and not 0");
+            } 
+
+    }
+}
