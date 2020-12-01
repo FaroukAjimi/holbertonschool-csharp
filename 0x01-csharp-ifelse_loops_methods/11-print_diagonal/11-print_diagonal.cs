2d302c5a21e7adfa8d65d7977ed605f65457e162
FaroukAjimi <benajimi@gmail.com>
2020-12-01 18:19:20 +0100

    Soo

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            string tabs = new String(' ', 0);
+            if (length > 0)
+            { 
+                for (int i = 0; i < length; i++)
+                {
+                    tabs = new String(' ', i);
+                    Console.Write(tabs);
+                    Console.WriteLine("\\");    
+                }
+            }    
+            Console.WriteLine();
+        }
+    }
