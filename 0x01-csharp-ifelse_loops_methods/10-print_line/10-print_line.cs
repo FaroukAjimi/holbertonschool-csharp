08f73ddf30b482a6cda613d8c29564f94c432591
FaroukAjimi <benajimi@gmail.com>
2020-12-01 17:20:29 +0100

    Soo

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+        for (int i = 0; i <= length; i++)
+         {
+          if (length > 0)
+            {
+                Console.Write("_");
+            }
+         }
+            Console.WriteLine();
+        }
+    }
