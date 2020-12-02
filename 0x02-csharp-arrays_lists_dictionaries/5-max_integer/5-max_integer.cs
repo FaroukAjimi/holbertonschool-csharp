aca011a961e978093471ce2bf6f415b0607b61d8
FaroukAjimi <benajimi@gmail.com>
2020-12-02 14:39:24 +0100

    sOO3 4 3 3

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
     {
         public static int MaxInteger(List<int> myList)
         { 
-            int max = 1;
+            int max = myList[0];
             if (myList.Count == 0)
             {
                 Console.WriteLine("List is empty");
