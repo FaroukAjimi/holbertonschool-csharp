4ebd15d06563794705ca957dd643833b039cead0
FaroukAjimi <benajimi@gmail.com>
2020-12-02 12:32:55 +0100

    Spp

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Array
+    {
+        public static void Reverse(int[] array)
+        {
+            int i = 0;
+            if (array == null || array.Length <= 0)
+            {
+                Console.WriteLine();
+                return;
+            }
+            for (i = array.Length - 1; i > 0; i--)
+            {
+                Console.Write(array[i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine(array[i]);
+        }
+    }
