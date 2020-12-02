1b62c999e37dd7be7ed8137ddfec7b364331f546
FaroukAjimi <benajimi@gmail.com>
2020-12-02 13:55:57 +0100

    Spp

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
             if (myList.Count == 0)
             {
                 Console.WriteLine("List is empty");
-                return(-1)
+                return(-1);
             }
             for (int i = 0; i < myList.Count; i++)
             {
