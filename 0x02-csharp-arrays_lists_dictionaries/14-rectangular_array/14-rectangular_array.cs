0a8a9dd9ae611a3ebe1fca7be6c4c6541a3876af
FaroukAjimi <benajimi@gmail.com>
2020-12-02 18:19:12 +0100

    Soo

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -6,7 +6,24 @@ namespace _14_rectangular_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 5; i++)
+            {
+                for(int y = 0; y < 5; y++)
+                {
+                    if (i == 2 && y == 2)
+                    {
+                        Console.Write("1 ");
+                        continue;
+                    }
+                    if (y == 4)
+                    {
+                        Console.Write("0");
+                        continue;
+                    }
+                    Console.Write("0 ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
