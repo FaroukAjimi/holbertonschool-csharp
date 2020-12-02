0b41fb229a0fa6a1dba59ff2d11ac98e5e7f4fc5
FaroukAjimi <benajimi@gmail.com>
2020-12-02 18:07:20 +0100

    Sp

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _13_best_score
-{
-    class Program
+    class Dictionary
     {
-        static void Main(string[] args)
+        public static string BestScore(Dictionary<string, int> myList)
         {
-            Console.WriteLine("Hello World!");
+            if (myList.Count == 0)
+                return("None");
+            List<int> l = new List<int>();
+            foreach(var item in myList)
+                l.Add(item.Value);
+            int i = l[0];
+            foreach(var y in l)
+            {
+                if (y > i)
+                    i = y;
+            }
+            string champ = "";
+            foreach(var z in myList)
+                {
+                    if (z.Value == i)
+                        champ = z.Key;
+                }
+            return(champ);
         }
     }
-}
