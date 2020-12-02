a553b5bc518e344baec7c2462bdcea7ef1bdd286
FaroukAjimi <benajimi@gmail.com>
2020-12-02 17:28:40 +0100

    Soo

--- 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _12_print_sorted_dictionary
-{
-    class Program
+    class Dictionary
     {
-        static void Main(string[] args)
+        public static void PrintSorted(Dictionary<string, string> myDict)
         {
-            Console.WriteLine("Hello World!");
+            List<string> l = new List<string>();
+            Dictionary<string, string> n = new Dictionary<string, string>();
+            foreach(var item in myDict)
+                l.Add(item.Key);
+            l.Sort();
+            foreach (var i in l)
+                Console.WriteLine("{0}: {1}", i, myDict[i]);          
         }
     }
-}
+
