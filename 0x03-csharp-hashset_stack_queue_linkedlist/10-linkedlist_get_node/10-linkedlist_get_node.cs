a22e5dad08d3c4d20ac123253381d2418485da0b
FaroukAjimi <benajimi@gmail.com>
2020-12-03 15:32:25 +0100

    Spp

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-
-namespace _10_linkedlist_get_node
-{
-    class Program
+using System.Collections.Generic;
+    class LList
     {
-        static void Main(string[] args)
+        public static int GetNode(LinkedList<int> myLList, int n)
         {
-            Console.WriteLine("Hello World!");
+            int  i = 0;
+            foreach(var itm in myLList)
+            {
+                if (i == n)
+                    return(itm);
+                i++;
+            }
+            return(0);
         }
     }
-}
