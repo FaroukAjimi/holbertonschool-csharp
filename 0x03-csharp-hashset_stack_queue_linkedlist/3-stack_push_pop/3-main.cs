edf5fb6135bb31ecd884294a06047d03d6b7ec01
FaroukAjimi <benajimi@gmail.com>
2020-12-03 13:20:17 +0100

    Soo

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-main.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Main - entry point
+    static void Main(string[] args)
+    {
+        Stack<string> aStack = new Stack<string>();
+
+
+        foreach (string item in aStack)
+            Console.WriteLine(item);
+
+        Console.WriteLine("------");
+
+        MyStack.Info(aStack, "C#", "Javascript");
+
+        Console.WriteLine("------");
+
+        foreach (string item in aStack)
+            Console.WriteLine(item);
+    }
+}
--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _3_stack_push_pop
-{
-    class Program
+    class MyStack
     {
-        static void Main(string[] args)
+        public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
         {
-            Console.WriteLine("Hello World!");
+            int c = 0;
+            bool chk = false;
+            string top = "";
+            int c2 = 0;
+            bool srch = false;
+            int index = 0;
+            foreach(var i in aStack)
+            {
+                if(chk is false)
+                {
+                    top = i;
+                    chk = true;
+                }
+                if(i == search)
+                {
+                    srch = true;
+                    index = c;
+                }
+                c += 1;
+            }
+            while(c2 < index)
+            {
+                aStack.Pop();
+                c2++;
+            }
+            aStack.Push(newItem);
+            Console.WriteLine("Number of items: {0}", c);
+            if (c > 0)
+                Console.WriteLine("Top item: {0}", top);
+            else
+                Console.WriteLine("Stack is empty");
+            Console.WriteLine("Stack contains \"{0}\": {1}", search, srch);
+            return(aStack);
         }
     }
-}
+
