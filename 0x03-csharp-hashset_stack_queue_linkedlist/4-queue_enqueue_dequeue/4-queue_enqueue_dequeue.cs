b0f469486553b534b4f6ea2782523bd17925be42
FaroukAjimi <benajimi@gmail.com>
2020-12-03 13:37:54 +0100

    In queue

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace _4_queue_enqueue_dequeue
-{
-    class Program
+using System.Collections.Generic;
+    class MyQueue
     {
-        static void Main(string[] args)
+        public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
         {
-            Console.WriteLine("Hello World!");
+             int c = 0;
+            bool chk = false;
+            string top = "";
+            int c2 = 0;
+            bool srch = false;
+            int index = 0;
+            foreach(var i in aQueue)
+            {
+                if(chk is false)
+                {
+                    top = i;
+                    chk = true;
+                }
+                if(i == search)
+                {
+                    srch = true;
+                    index = c;
+                }
+                c += 1;
+            }
+            if (srch == true)
+                while(c2 <= index)
+                {
+                    aQueue.Dequeue();
+                    c2++;
+                }
+            aQueue.Enqueue(newItem);
+            Console.WriteLine("Number of items: {0}", c);
+            if (c > 0)
+                Console.WriteLine("Top item: {0}", top);
+            else
+                Console.WriteLine("Queue is empty");
+            Console.WriteLine("Queue contains \"{0}\": {1}", search, srch);
+            return(aQueue);
         }
     }
-}
