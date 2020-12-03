28bc81356ff1976ea261fb2db18ed8b624828006
FaroukAjimi <benajimi@gmail.com>
2020-12-03 16:59:40 +0100

    Soo

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _13_linkedlist_delete
-{
-    class Program
+    class LList
     {
-        static void Main(string[] args)
+        public static void Delete(LinkedList<int> myLList, int index)
         {
-            Console.WriteLine("Hello World!");
+            List<int> l = new List<int>();
+            int c = 0;
+            int c2 = 0;
+            foreach(var itm in myLList)
+             {
+                if (c2 == index)
+                {
+                    c2++;
+                    continue;
+                }
+                l.Add(itm);
+                c2++;
+             }
+            c = myLList.Count;
+            for(int i = 1; i <= c; i++)
+                myLList.RemoveFirst();         
+            foreach(var it in l)
+            {
+                myLList.AddLast(it);
+            }
         }
     }
-}
+
