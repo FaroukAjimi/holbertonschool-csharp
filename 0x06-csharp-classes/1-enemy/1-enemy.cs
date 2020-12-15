1b86858462b2130eaaf48b0bd21c29327bae7c43
FaroukAjimi <benajimi@gmail.com>
2020-12-15 14:32:17 +0100

    so'

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Enemies class
+*/
+namespace Enemies
+{
+    /// <summary>
+    /// this class does nothing
+    /// </summary>
+    class Zombie
+    {
+        public int? health {get; set;} = null;
+        
+        /// zombie init health to 0
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
