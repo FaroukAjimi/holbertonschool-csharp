e3ea5517b48b049ce2be18c5186ad80ee1c91a6b
FaroukAjimi <benajimi@gmail.com>
2020-12-15 14:52:06 +0100

    Soo

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+/*
+    Enemies class
+*/
+namespace Enemies
+{
+    /// <summary>
+    /// this class does nothing
+    /// </summary>
+    class Zombie
+    {
+        /// public field health
+        public int? health {get; set;} = null;
+
+        /// zombie init health to 0
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// 2nd public constructor zombie
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+    }
+}
