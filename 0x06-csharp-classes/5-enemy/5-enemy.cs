8d57fff89944ff6ae351a0d53b4c61e16da92f60
FaroukAjimi <benajimi@gmail.com>
2020-12-15 16:47:00 +0100

    SOo

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+/*
+    Enemies class
+*/
+namespace Enemies
+{
+    /// <summary>
+    /// this class does nothing
+    /// </summary>
+    class Zombie
+    {
+        /// public field health
+        private int health;
+        /// private name
+        private string name = "(No name)";
+        /// pub property
+        public string Name
+        {
+            get { return name;}
+            set { name = value;}
+        }
+        /// zombie init health to 0
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// 2nd public constructor zombie
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+        // getter health
+        public int GetHealth()
+        {
+            return(health);
+        }
+        /// override tostring
+        public override string ToString()
+        {
+            return ("Zombie name " + name + " / Total Health: " + health);
+        }
+    }
+}
