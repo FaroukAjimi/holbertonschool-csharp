d0c37530c48b4aac45046a2c51e754aa317386e7
FaroukAjimi <benajimi@gmail.com>
2020-12-19 21:21:02 +0100

    SOo

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+    class Obj
+    {
+        public static void Print(object myObj)
+        {
+            TypeInfo t =  myObj.GetType().GetTypeInfo();
+            IEnumerable<PropertyInfo> l = t.GetProperties();
+            IEnumerable<MethodInfo> m = t.GetMethods();
+        Console.WriteLine(t.Name + " Properties:");
+           foreach(PropertyInfo i in l)
+                Console.WriteLine(i.Name);
+        Console.WriteLine(t.Name + " Methods:");
+           foreach (MethodInfo x in m)
+                    Console.WriteLine(x.Name);
+        }
+    }
