f5b90f2c7310ef87244bdf1543395bc0358bf4ed
FaroukAjimi <benajimi@gmail.com>
2020-12-21 13:27:35 +0100

    Soo

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+    /// <summary> class obj </summary>
+    
     class Obj
     {
+        /// <summary> check instance </summary>
          public static bool IsInstanceOfArray(object obj)
         {
            if (typeof(Array).IsInstanceOfType(obj))
