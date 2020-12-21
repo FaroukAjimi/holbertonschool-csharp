1d43e35e63055eb108d94d188c8546202580d4e1
FaroukAjimi <benajimi@gmail.com>
2020-12-21 13:18:44 +0100

    Check task 1

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
+    /// <summary> class obj </summary> 
     class Obj
     {
+        /// <summary> check if type is int method </summary>
         public static bool IsOfTypeInt(object obj)
         {
             if (obj is int)
