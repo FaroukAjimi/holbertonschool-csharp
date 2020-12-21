26270ddbed74ef504a413355e668dad5c2cc16b5
FaroukAjimi <benajimi@gmail.com>
2020-12-21 13:52:40 +0100

    Soo

--- 0x08-csharp-inheritance/9-shape/9-shape.cs
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -1,38 +1,47 @@
 ﻿using System;
 using System.Reflection;
+    /// <summary> shape </summary>
     class Shape
     {
+        /// <summary> area </summary>
         public virtual int Area()
         {
         throw new NotImplementedException("Area() is not implemented");
         }
     }
+    /// <summary> rec inh(shape) </summary>
     class Rectangle : Shape {
         private int width;
         private int height;
+        /// <summary> int widht </summary>
         public int Width {
             get{return width;}
             set{if (value < 0)
             {throw new ArgumentException("Width must be greater than or equal to 0");
             }else{width = value;}}
         }
+        /// <summary>int height</summary>
         public int Height {
             get{return height;}
             set{if (value < 0)
             {throw new ArgumentException("Height must be greater than or equal to 0");
             }else{height = value;}}
         }
+        /// <summary> se area </summary>
         public new int Area(){
                 return(this.width * this.height);
         }
+        /// <summary> override string</summary>
         public override string ToString()
         {
             return string.Format("[{0}] {1} / {2}",this.GetType(), width, height);
         }
     }
+    /// <summary> square inh(rec) </summary>
     class Square : Rectangle
     {
         private int size;
+        /// <summary> int size</summary>
         public int Size{
             get{return size;}
             set{
