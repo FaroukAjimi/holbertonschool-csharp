a10d42772b75ca3b53730c876028bb5bf803c527
FaroukAjimi <benajimi@gmail.com>
2020-12-21 13:40:28 +0100

    OSoo' g

--- 0x08-csharp-inheritance/5-shape/5-shape.cs
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 
-
+    /// <summary> class shape </summary>
     class Shape
     {
+        /// <summary> public virtual method </summary>
         public virtual int Area()
         {
         throw new NotImplementedException("Area() is not implemented");
