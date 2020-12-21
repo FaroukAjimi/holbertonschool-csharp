e8e724b31c3e3bbf4ceaacfd86d172d5af581390
FaroukAjimi <benajimi@gmail.com>
2020-12-21 13:42:20 +0100

    Soo

--- 0x08-csharp-inheritance/6-shape/6-shape.cs
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 
-
+    /// <summary> </summary>
     class Shape
     {
+        /// <summary> </summary>
         public virtual int Area()
         {
         throw new NotImplementedException("Area() is not implemented");
         }
     }
+    /// <summary> class rectangle inh(shape) </summary>
     class Rectangle : Shape {
         private int width;
         private int height;
+        /// <summary> public int width </summary>
         public int Width {
             get{return width;}
             set{if (value < 0)
             {throw new ArgumentException("Width must be greater than or equal to 0");
             }else{width = value;}}
         }
+        /// <summary> public height </summary>
         public int Height {
             get{return height;}
             set{if (value < 0)
@@ -24,4 +28,3 @@
             }else{height = value;}}
         }
     }
-    
