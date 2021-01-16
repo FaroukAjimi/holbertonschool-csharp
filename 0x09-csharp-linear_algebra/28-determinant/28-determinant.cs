6f3288d5050a74a63ea937fbe1997cb57378de8a
FaroukAjimi <benajimi@gmail.com>
2021-01-16 18:47:31 +0100

    Soo

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -14,7 +14,7 @@ class MatrixMath{
             double s1 = matrix[0,0] * (matrix[1,1] * matrix[2,2] - matrix[1,2] * matrix[2,1]);
             double s2 = matrix[0,1] * (matrix[1,0] * matrix[2,2] - matrix[1,2] * matrix[2,0]);
             double s3 = matrix[0,2] * (matrix[1,0] * matrix[2,1] - matrix[1,1] * matrix[2,0]);
-            return(Math.Round(s1 - s2 + s3));
+            return(Math.Round(s1 - s2 + s3, 2));
         }
         else
             return(-1);
