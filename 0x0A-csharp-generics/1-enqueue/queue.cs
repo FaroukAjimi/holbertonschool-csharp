22aa3326d46ba0ebf0ebc0ac0f2c040295523806
FaroukAjimi <benajimi@gmail.com>
2021-03-22 17:09:11 +0100

    SOo

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+///<summary> generic class Queue </summary>
+class Queue<T>
+{
+    Node head {get; set;}
+    Node tail {get; set;}
+    int count {get; set;}
+
+    ///<summary> class method </summary>
+    public Type CheckType()
+    {
+        return(typeof(T));
+    }
+    public void Enqueue(T val) {
+        if (head == null)
+        {
+            Node newHead = new Node();
+            newHead.value = val;
+            head = tail = newHead;
+        }
+        else{
+            Node NewTail = new Node();
+            NewTail.value = val;
+            tail.next = NewTail;
+            tail = NewTail;
+        }
+        count++;
+    }
+    public int Count(){
+        return count;
+        }
+public class Node{
+    public T value {get; set;} = default;
+    public Node next {get; set;} = default;
+    
+    // public Node(T value)
+    //     {
+    //         this.value = value;
+    //     }
+} 
+}
+
+
+
