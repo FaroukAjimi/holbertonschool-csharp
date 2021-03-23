68efeb747acc81b07869f95e30119b2d75ccefc3
FaroukAjimi <benajimi@gmail.com>
2021-03-23 22:59:38 +0100

    So

--- 0x0B-csharp-interfaces/3-decorations/main.cs
+++ 0x0B-csharp-interfaces/3-decorations/main.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Decoration coffeeMug = new Decoration("Coffee Mug", 4);
+        Decoration coffeeMug = new Decoration("Coffee Mug", 2);
 
         Console.WriteLine(coffeeMug.ToString());
 
