ba12783fa2c35ed5ff0c5de94e10e68f026f2e3e
FaroukAjimi <benajimi@gmail.com>
2021-03-23 21:34:15 +0100

    oo

--- 0x0B-csharp-interfaces/1-user_interface/main.cs
+++ 0x0B-csharp-interfaces/1-user_interface/main.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Reflection;
 
 class Program
@@ -10,10 +9,6 @@ class Program
         Type type = item.GetType();
 
         Console.WriteLine("Type: " + type);
-
-        Console.WriteLine("Properties:");
-        foreach (PropertyInfo info in type.GetProperties())
-            Console.WriteLine(info.Name);
 
         Console.WriteLine("Methods:");
         foreach (MethodInfo info in type.GetMethods())
