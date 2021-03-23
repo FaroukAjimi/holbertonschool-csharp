1765967ca0669cdc4598b3c1838bbc3931e4fd1d
FaroukAjimi <benajimi@gmail.com>
2021-03-23 04:52:15 +0100

    Soo

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+    ///<summary> base class </summary>
+    public abstract class Base
+    {
+     ///<summary> overrider </summary>
+    public String name {get; set;}
+     ///<summary> overrider </summary>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+    }
+     ///<summary> overrider </summary>
+    public interface IInteractive {
+         ///<summary> overrider </summary>
+        void Interact();
+    }
+     ///<summary> overrider </summary>
+    public interface IBreakable{
+         ///<summary> overrider </summary>
+         int durability {get; set;}
+          ///<summary> overrider </summary>
+         void Break();
+    }
+     ///<summary> overrider </summary>
+    public interface ICollectable{
+         ///<summary> overrider </summary>
+        bool isCollected {get; set;}
+         ///<summary> overrider </summary>
+        void Collect();
+    }
+     ///<summary> overrider </summary>
+    public class Door : Base, IInteractive{
+    ///<summary> overrider </summary>
+    public Door(string value){
+            value = "Door";
+            this.name = value;
+    }
+     ///<summary> overrider </summary>
+    public void Interact(){
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+}
