21410049a922e43d5c1059301f14e75e27b93c21
FaroukAjimi <benajimi@gmail.com>
2021-03-23 04:35:41 +0100

    Soo

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+    ///<summary> base class </summary>
+    public abstract class Base
+    {
+     ///<summary> overrider </summary>
+    public String name {get; set;}
+     ///<summary> overrider </summary>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+    }
+     ///<summary> overrider </summary>
+    public interface IInteractive {
+         ///<summary> overrider </summary>
+        void Interact();
+    }
+     ///<summary> overrider </summary>
+    public interface IBreakable{
+         ///<summary> overrider </summary>
+         int durability {get; set;}
+          ///<summary> overrider </summary>
+         void Break();
+    }
+     ///<summary> overrider </summary>
+    public interface ICollectable{
+         ///<summary> overrider </summary>
+        bool isCollected {get; set;}
+         ///<summary> overrider </summary>
+        void Collect();
+    }
+     ///<summary> overrider </summary>
+    public class TestObject : Base, IInteractive, IBreakable, ICollectable{
+         ///<summary> overrider </summary>
+        public void Interact(){} 
+         ///<summary> overrider </summary>
+        public int durability {get; set;}
+         ///<summary> overrider </summary>
+        public void Break(){}
+         ///<summary> overrider </summary>
+        public bool isCollected {get; set;}
+         ///<summary> overrider </summary>
+        public void Collect(){}
+    }
