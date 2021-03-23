9eab0c2b2014983766080430e56d2d49881cc88c
FaroukAjimi <benajimi@gmail.com>
2021-03-23 21:11:35 +0100

    ooo

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,4 +1,61 @@
-﻿using System;
+﻿// using System;
+
+//     ///<summary> base class </summary>
+//     public abstract class Base
+//     {
+//      ///<summary> overrider </summary>
+//     public string name {get; set;}
+//      ///<summary> overrider </summary>
+//     public override string ToString()
+//     {
+//         return $"{name} is a {this.GetType()}";
+//     }
+//     }
+
+
+//      ///<summary> overrider </summary>
+//     public interface IInteractive {
+//          ///<summary> overrider </summary>
+//         void Interact();
+//     }
+//      ///<summary> overrider </summary>
+//     public interface IBreakable{
+//          ///<summary> overrider </summary>
+//          /// <value>Getter and setter</value>
+//          int durability {get; set;}
+//           ///<summary> overrider </summary>
+//          void Break();
+//     }
+//      ///<summary> overrider </summary>
+//     public interface ICollectable{
+//          ///<summary> overrider </summary>
+//          /// <value>Getter and setter</value>
+//         bool isCollected {get; set;}
+//          ///<summary> overrider </summary>
+//         void Collect();
+//     }
+//      ///<summary> overrider </summary>
+//     public class TestObject : Base, IInteractive, IBreakable, ICollectable{
+//          ///<summary> overrider </summary>
+//          /// <value>Getter and setter</value>
+//         public int durability {get; set;}
+//         ///<summary> overrider </summary>
+//         /// <value>Getter and setter</value>
+//         public bool isCollected {get; set;}
+//         ///<summary> overrider </summary>
+//         public void Interact(){
+//             // Method implementation.
+//         } 
+//          ///<summary> overrider </summary>
+//         public void Break(){
+//             // Method implementation.
+//         }   
+//          ///<summary> overrider </summary>
+//         public void Collect(){
+//             // Method implementation.
+//         }
+//     }
+using System;
 
     ///<summary> base class </summary>
     public abstract class Base
@@ -55,3 +112,4 @@
             // Method implementation.
         }
     }
+
