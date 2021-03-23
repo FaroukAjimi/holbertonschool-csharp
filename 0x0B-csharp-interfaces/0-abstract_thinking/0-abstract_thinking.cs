17984b7754f33205826d99585dcb8d95eaa9b1c5
FaroukAjimi <benajimi@gmail.com>
2021-03-23 03:42:52 +0100

    SOo

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    ///<summary> base class </summary>
+    class Base
+    {
+    // properties
+    public String name {get; set;}
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+    }
